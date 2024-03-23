1f5e8774b3b54d3f4004e8819325041246c1f549
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-03-24 00:03:08 +0530

    Fixed tags styling

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Tag({name}) {
   return (
-    <span className='rounded-full bg-indigo-100 px-2 py-1 text-xs font-medium text-indigo-500'>{name}</span>
+    <span className='rounded-full bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-400'>{name}</span>
   )
 }
 
