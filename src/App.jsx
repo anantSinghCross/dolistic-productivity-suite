a639a80e33f5260f6b1f25e2a772975576d93bea
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-03-28 23:11:09 +0530

    Minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TaskManager from "./taskManager/TaskManager";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./common/Header";
 import NotesManager from "./notesManager/NotesManager";
 
