6965a117de7751714544371ac50ebe0744bcafce
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-03-29 02:04:40 +0530

    Minor

--- src/notesManager/NotesManager.jsx
+++ src/notesManager/NotesManager.jsx
@@ -5,9 +5,10 @@ import 'draft-js/dist/Draft.css';
 function NotesManager() {
   const [ editorState, setEditorState ] = useState(() => EditorState.createEmpty());
   return (
-    <>
+    <div className=' mr-2 ml-2 p-2 rounded-lg border-2'>
+      <p>Under Development ⚒️</p>
       <Editor editorState={editorState} onChange={setEditorState}/>
-    </>
+    </div>
   )
 }
 
